73b346d571ea710a3d398c906fa0e051baca252c
Jess <jessicadinh05@gmail.com>
2022-11-02 04:11:44 +1100

    Adding more search results

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import style from './App.module.scss'
+import  { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useState, useContext } from 'react'
 import BookContext from './components/BookContext/BookContext';
-import  { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PageContext from './components/PageContext/PageContext';
 import Header from './containers/Header/Header';
 import Results from './containers/Results/Results';
 import Footer from './components/Footer/Footer';
@@ -9,19 +10,24 @@ import Footer from './components/Footer/Footer';
 
 function App() {
   const [books, setBooks] = useState([]);
+  const [totalItems, setTotalItems] = useState([]);
+  const [page, setPage] = useState([]);
+  const [startIndex, setStartIndex] = useState([]);
 
   return (
     <div className={style.container}>
       <div className={style.content}>
         <BookContext.Provider value={[books, setBooks]}>
-          <BrowserRouter basename="/book-api">
-            <Header/>
-            <Routes>
-              <Route path='/book-api/'></Route>
-              <Route path='/results' element={<Results />}></Route>
-            </Routes>
-            <Footer />
-          </BrowserRouter>
+            <PageContext.Provider value={[totalItems, setTotalItems, page, setPage, startIndex, setStartIndex]}>
+            <BrowserRouter basename="/book-api">
+              <Header/>
+              <Routes>
+                <Route path='/book-api/'></Route>
+                <Route path='/results' element={<Results />}></Route>
+              </Routes>
+              <Footer />
+            </BrowserRouter>
+          </PageContext.Provider>
         </BookContext.Provider>
       </div>
     </div>
